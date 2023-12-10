3bbac4db1582bf368578ed4f464b971a3baa69b2
Joon Park <hyojoon2372552@naver.com>
2023-12-11 05:57:35 +1100

    [level unrated] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,7 +1,9 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
-        for(int i = num_list.length-1, j=0; i >= 0; i--, j++) answer[i] = num_list[j];
+        for(int i = 0; i< num_list.length; i++){
+            answer[i] = num_list[num_list.length-i-1];
+        }
         return answer;
     }
 }
