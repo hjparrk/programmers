335677d661b9ac1ea41e54fe8c2155cc9f046b49
Joon Park <hyojoon2372552@naver.com>
2023-12-11 04:15:30 +1100

    [level unrated] Title: 최빈값 구하기, Time: 0.13 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -2,36 +2,22 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(int[] array) {
+        
         int answer = 0;
-        int maxAns = 0;
+        int maxCount = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            if (map.isEmpty() || !map.containsKey(array[i])) {
-                map.put(array[i], 1);
-            } else {
-                int value = map.get(array[i]);
-                map.put(array[i], value + 1);
+        
+        for(int number : array){
+            int count = map.getOrDefault(number, 1) + 1;
+            if(count > maxCount){
+                maxCount = count;
+                answer = number;
             }
-        }
-        for (int key : map.keySet()) {
-            int value = map.get(key);
-            if (value > answer) {
-                answer = value;
-                maxAns = key;
-            } else if (value == answer) {
+            else  if(count == maxCount){
                 answer = -1;
-                maxAns = -1;
-                for (int subKey : map.keySet()) {
-                    int subValue = map.get(subKey);
-                    if (value == subValue) {
-                        map.put(key, -1);
-                        break;
-                    }
-                }
             }
+            map.put(number, count);
         }
-        return maxAns;
-
-
+        return answer;
     }
 }
