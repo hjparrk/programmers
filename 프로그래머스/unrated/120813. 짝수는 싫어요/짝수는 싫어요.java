7651f494373068a5682c3936c4727eee64d162f2
Joon Park <hyojoon2372552@naver.com>
2023-12-11 04:33:52 +1100

    [level unrated] Title: 짝수는 싫어요, Time: 0.21 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] solution(int n) {
         
-        int[] answer = new int[(int)Math.ceil(n/2.0)];
+        int[] answer = new int[(int) Math.ceil(n / 2.0)];
         
         for(int i = 0, odd = 1; i < answer.length; i++, odd += 2) {
             answer[i] = odd;
