7f944d44cfd50822ad2e5f5cdb091d20bfb6b43d
Joon Park <hyojoon2372552@naver.com>
2023-12-12 05:22:29 +1100

    [level unrated] Title: 배열의 유사도, Time: 0.06 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.java
@@ -1,12 +1,19 @@
-import java.util.HashMap;
+// import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 class Solution {
     public int solution(String[] s1, String[] s2) {
-        HashMap<String, Boolean> map = new HashMap<>();
+        // HashMap<String, Boolean> map = new HashMap<>();
+        // int counter = 0;
+        // for(String s : s1) map.put(s, false);
+        // for(String s : s2) if(map.containsKey(s)) map.put(s, true);
+        // for(String key : map.keySet()) if(map.get(key)) counter++;
+        // return counter;
+        
+        HashSet<String> set = new HashSet<>(List.of(s1));
         int counter = 0;
-        for(String s : s1) map.put(s, false);
-        for(String s : s2) if(map.containsKey(s)) map.put(s, true);
-        for(String key : map.keySet()) if(map.get(key)) counter++;
+        for (String s : s2) if(set.contains(s)) counter++;
         return counter;
     }
 }
