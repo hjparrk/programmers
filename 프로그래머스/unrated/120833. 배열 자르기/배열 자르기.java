e714a2a96afac3ceaab8aade87ac873852ca1e4e
Joon Park <hyojoon2372552@naver.com>
2023-12-12 05:11:30 +1100

    [level unrated] Title: 배열 자르기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -3,5 +3,7 @@ class Solution {
         int[] answer = new int[num2-num1+1];
         for(int i = num1, j=0; i <= num2; i++, j++) answer[j] = numbers[i];
         return answer;
+        
+        // return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
