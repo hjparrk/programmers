f3e8e072bbd14f4b6f8da91c1286ef2b4d04c25c
Joon Park <hyojoon2372552@naver.com>
2023-12-12 06:13:42 +1100

    [level unrated] Title: 자릿수 더하기, Time: 5.11 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java
@@ -3,5 +3,12 @@ class Solution {
         int sum = 0;
         for(char c : ("" + n).toCharArray()) sum += Character.getNumericValue(c);
         return sum;
+        
+        // int answer = 0;
+        // while(n != 0){
+        //     answer += (n%10);
+        //     n = n/10;
+        // }
+        // return answer;
     }
 }
