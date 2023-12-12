b5973aa03735e31dd093aab142568c5354403123
Joon Park <hyojoon2372552@naver.com>
2023-12-13 05:14:57 +1100

    [level unrated] Title: 외계행성의 나이, Time: 1.22 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,14 +1,24 @@
-import java.util.HashMap;
+// import java.util.HashMap;
 
 class Solution {
     public String solution(int age) {
+        // String answer = "";
+        // String[] array = "a b c d e f g h i j".split(" ");
+        // HashMap<Integer,String> map = new HashMap<>();
+        // for(int i = 0; i < array.length; i++) map.put(i, array[i]);
+        // for(char c : String.valueOf(age).toCharArray()) {
+        //     answer += map.get(Character.getNumericValue(c));
+        // }
+        // return answer;
+        
         String answer = "";
-        String[] array = "a b c d e f g h i j".split(" ");
-        HashMap<Integer,String> map = new HashMap<>();
-        for(int i = 0; i < array.length; i++) map.put(i, array[i]);
-        for(char c : String.valueOf(age).toCharArray()) {
-            answer += map.get(Character.getNumericValue(c));
+        String[] alpha = new String[]{"a","b","c","d","e","f","g","h","i","j"};
+
+        while(age>0){
+            answer = alpha[age % 10] + answer;
+            age /= 10;
         }
+
         return answer;
     }
 }
