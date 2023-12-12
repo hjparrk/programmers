7dd8da0dbc6487fe19b48b957c559235fb146008
Joon Park <hyojoon2372552@naver.com>
2023-12-13 05:52:50 +1100

    [level unrated] Title: 모스부호 (1), Time: 9.11 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] word = letter.split(" ");
+
+        for(int i=0;i<word.length;i++){
+            for(int j=0;j<morse.length;j++){
+                if(word[i].equals(morse[j])) answer+=(char)(j+97); //아스키코드
+            }
+        }
+        return answer;
+    }
+}
