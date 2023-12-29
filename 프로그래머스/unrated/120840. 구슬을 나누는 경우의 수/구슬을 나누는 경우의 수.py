fd22ca33e0ed2a800581eb15b17d3ca4737cc7f6
Hyojoon Park <hpar9758@naver.com>
2023-12-30 04:59:31 +1100

    [level unrated] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,10 +1,20 @@
-import math
+# import math
+
+# def solution(balls, share):
+#     # memo = [i for i in range(balls+1)]
+#     # memo[0] = 1
+#     # for i in range(2, len(memo)):
+#     #     memo[i] *= memo[i-1]        
+#     # return memo[balls] / (memo[balls - share] * memo[share])
+    
+#     return math.comb(balls, share)
 
 def solution(balls, share):
-    # memo = [i for i in range(balls+1)]
-    # memo[0] = 1
-    # for i in range(2, len(memo)):
-    #     memo[i] *= memo[i-1]        
-    # return memo[balls] / (memo[balls - share] * memo[share])
-    
-    return math.comb(balls, share)
+    answer = factorial(balls) / (factorial(balls - share) * factorial(share))
+    return answer
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+    return result
