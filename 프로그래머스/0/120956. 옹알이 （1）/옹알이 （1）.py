6f21dd8f831a8f4ed9ed8127b8ec159ce7cece0f
Hyojoon Park <hpar9758@naver.com>
2024-11-06 04:03:58 +1100

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,11 +1,9 @@
+import re
+
 def solution(babbling):
-    answer = 0
-    words = ["aya", "ye", "woo", "ma"]
-    for s in babbling:
-        length = len(s)
-        for word in words:
-            if word in s:
-                s = s.replace(word, "*" * len(word))
-        if s == "*" * length:
-            answer += 1
-    return answer
+    regex = re.compile('^(aya|ye|woo|ma)+$')
+    ans = 0
+    for e in babbling:
+        if regex.match(e):
+            ans += 1
+    return ans
